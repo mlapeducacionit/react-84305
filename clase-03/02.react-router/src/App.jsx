e5e492ce670f0d4f3554beaaaf580fd66640d1c5
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-10-22 16:28:43 -0300

    Agregamos el router a la app react

--- clase-03/02.react-router/src/App.jsx
+++ clase-03/02.react-router/src/App.jsx
@@ -1,8 +1,27 @@
 // rafce
 
+import { BrowserRouter, Route, Routes } from "react-router"
+import Inicio from "./pages/Inicio"
+import Producto from "./pages/Producto"
+import Nosotros from "./pages/Nosotros"
+import Contacto from "./pages/Contacto"
+import NoEncontrado from "./pages/NoEncontrado"
+
 const App = () => {
+
   return (
-    <div>App</div>
+    <BrowserRouter>
+
+        <Routes> {/* Switch */}
+            <Route path="/" element={<Inicio />} /> {/* Case */}
+            <Route path="/productos" element={<Producto />} /> {/* Case */}
+            <Route path="/nosotros" element={<Nosotros />} /> {/* Case */}
+            <Route path="/contacto" element={<Contacto />} /> {/* Case */}
+            <Route path="*" element={<NoEncontrado />} /> {/* Case */}
+        </Routes>
+
+    </BrowserRouter>
+
   )
 }
 
