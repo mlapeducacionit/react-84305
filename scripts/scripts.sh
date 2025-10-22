bd1fa9c43120350848a8e61c2cf253676078c476
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-10-22 16:22:36 -0300

    Agregando las pages a nuestra aplicaicón

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -1,9 +1,14 @@
 print 'n\n' | npm create vite@latest ./ -- --template react
 npm i
 # git init
+# Limpiando el proyecto
+rm public/vite.svg src/assets/react.svg src/App.css
+echo "" > src/index.css # Limpiando de contenido el archivo
+echo "" > src/App.jsx # limpiando de contenido el archivo
 # https://tailwindcss.com/docs/installation/using-vite
 # Dependencias Tailwind
 npm i tailwindcss @tailwindcss/vite
+echo "@import 'tailwindcss';" > src/index.css
 # https://reactrouter.com/
 # Dependencias de React Router
 npm i react-router
