04e5b5ecddab26a32477ece0f506ef5aa481a088
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-10-22 17:02:20 -0300

    Terminamos con la navbar

--- clase-03/02.react-router/src/components/Navbar.jsx
+++ clase-03/02.react-router/src/components/Navbar.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+
 import { NavLink } from 'react-router'
+
+
+/* 
+isActive
+      ? 'text-blue-600 font-semibold'
+      : 'text-gray-700 hover:text-blue-600 transition-colors';
+*/
+//const cambioActivo = (props) => (props.isActive ? 'text-red-600 font-semibold' : 'text-gray-700 hover:text-red-600 transition-colors')
+const cambioActivo = ({isActive}) => (isActive ? 'text-red-600 font-semibold' : 'text-gray-700 hover:text-red-600 transition-colors')
 
 const Navbar = () => {
   return (
-    <nav>
-        <ul>
-            <li><NavLink to="/">Inicio</NavLink></li>
-            <li><NavLink to="/productos">Productos</NavLink></li>
-            <li><NavLink to="/nosotros">Nosotros</NavLink></li>
-            <li><NavLink to="/contacto">Contacto</NavLink></li>
-        </ul>
+     <nav className="bg-white shadow-md">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+          <div className="flex items-center space-x-6">
+            <NavLink to="/" className={cambioActivo}>Inicio</NavLink>
+            <NavLink to="/productos" className={cambioActivo}>Productos</NavLink>
+            <NavLink to="/nosotros" className={cambioActivo}>Nosotros</NavLink>
+            <NavLink to="/contacto" className={cambioActivo}>Contacto</NavLink>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
