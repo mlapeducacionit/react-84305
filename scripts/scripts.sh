ba58987da4da0ed581d3efa5d37d41b517bcf8c3
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-10-22 15:58:12 -0300

    Agregando el scripts a nuestro curso

---
+++ scripts/scripts.sh
@@ -0,0 +1,9 @@
+print 'n\n' | npm create vite@latest ./ -- --template react
+npm i
+# git init
+# https://tailwindcss.com/docs/installation/using-vite
+# Dependencias Tailwind
+npm i tailwindcss @tailwindcss/vite
+# https://reactrouter.com/
+# Dependencias de React Router
+npm i react-router
