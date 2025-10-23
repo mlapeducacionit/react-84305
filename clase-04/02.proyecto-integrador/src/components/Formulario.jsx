472dcfbb4fa5a1378cf2de7578e5cc48cc95242e
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-10-23 16:44:45 -0300

    Agrego los botones de limpiar y guardar en el formulario

--- clase-04/02.proyecto-integrador/src/components/Formulario.jsx
+++ clase-04/02.proyecto-integrador/src/components/Formulario.jsx
@@ -54,6 +54,11 @@ const Formulario = () => {
             />
         </div>
 
+        <div className="flex gap-3 mb-6">
+            <button type="reset" className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 cursor-pointer">Limpiar</button>
+            <button type="submit" className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 cursor-pointer">Guardar</button>
+        </div>
+
         </form>
     </>
   )
