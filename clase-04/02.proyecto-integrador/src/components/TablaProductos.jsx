a41eaee747a867c9c719663f22472a0831adb2ab
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-10-23 16:41:23 -0300

    Ajuste del responsive de la tabla

--- clase-04/02.proyecto-integrador/src/components/TablaProductos.jsx
+++ clase-04/02.proyecto-integrador/src/components/TablaProductos.jsx
@@ -4,22 +4,23 @@ const TablaProductos = () => {
   return (
     <>
         <h2>Listado de productos</h2>
+        <div className="overflow-x-auto w-full">
+            <table>
 
-        <table className="overflox-x-auto">
+                <thead className="min-w-full text-sm text-left text-gray-700 border border-gray-300 rounded-lg shadow-md">
+                    <tr className="bg-gray-100 text-gray-900 uppercase text-xs font-semibold">
+                        <th className="px-4 py-3 border-b">Nombre del producto</th>
+                        <th className="px-4 py-3 border-b">Categoría</th>
+                        <th className="px-4 py-3 border-b">Precio</th>
+                        <th className="px-4 py-3 border-b">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200">
+                    <ItemProducto />
+                </tbody>
 
-            <thead className="min-w-full text-sm text-left text-gray-700 border border-gray-300 rounded-lg shadow-md">
-                <tr className="bg-gray-100 text-gray-900 uppercase text-xs font-semibold">
-                    <th className="px-4 py-3 border-b">Nombre del producto</th>
-                    <th className="px-4 py-3 border-b">Categoría</th>
-                    <th className="px-4 py-3 border-b">Precio</th>
-                    <th className="px-4 py-3 border-b">Acciones</th>
-                </tr>
-            </thead>
-            <tbody className="divide-y divid-gray-200">
-                <ItemProducto />
-            </tbody>
-
-        </table>
+            </table>
+        </div>
     
     </>
     
