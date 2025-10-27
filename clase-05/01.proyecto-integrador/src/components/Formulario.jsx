a51d766e656314307ede191cb5b473e65f1d0cdb
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-10-27 15:48:20 -0300

    conectamos al boton reset la logica de reseteo

--- clase-05/01.proyecto-integrador/src/components/Formulario.jsx
+++ clase-05/01.proyecto-integrador/src/components/Formulario.jsx
@@ -33,7 +33,7 @@ const Formulario = () => {
   }
 
   const handleReset = () => {
-
+    setForm(formInicial)
   }
 
   return (
@@ -100,8 +100,8 @@ const Formulario = () => {
         </div>
 
         <div className="flex gap-3 mb-6">
-            <button type="reset" className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 cursor-pointer">Limpiar</button>
-            <button type="submit" className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 cursor-pointer">Guardar</button>
+            <button onClick={handleReset} type="reset" className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 cursor-pointer">Limpiar</button>
+            <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 cursor-pointer">Guardar</button>
         </div>
 
         </form>
