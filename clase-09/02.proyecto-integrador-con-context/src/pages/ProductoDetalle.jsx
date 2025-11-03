b97b0bd76c75bd0cf31351a671ba6d5667584e2c
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-11-03 17:04:29 -0300

    Terminado el detalle producto

--- clase-09/02.proyecto-integrador-con-context/src/pages/ProductoDetalle.jsx
+++ clase-09/02.proyecto-integrador-con-context/src/pages/ProductoDetalle.jsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router"
 
 const ProductoDetalle = () => {
+
+    const {id} = useParams() // { id }
+    // console.log(obj)
+    const [producto, setProducto] = useState(null)
+
+    useEffect(() => {
+      console.log('El componente se montó')
+      peticionProductoPorId(id)
+    }, [])
+    
+    const peticionProductoPorId = async (id) => {
+        const url = import.meta.env.VITE_BACKEND
+        const urlPorId = url + id
+        console.log(urlPorId)
+
+        try {
+            const res = await fetch(urlPorId)
+
+            if (!res.ok) {
+                throw new Error('No se pudo obtener el producto')
+            }
+
+            const productoPorId = await res.json()
+            console.log(productoPorId)
+            setProducto(productoPorId)
+            
+        } catch (error) {   
+            console.error(error)
+        }
+
+    }
+
   return (
-    <div>ProductoDetalle</div>
+    <>
+        <h2>Producto Detalle</h2>
+        { producto && <h3>Nombre: { producto.nombre }</h3> }
+    </>
   )
 }
 
